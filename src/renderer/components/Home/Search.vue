0f2e4cf3a277b021a0de31c65274f99437dd9ff7
zerodois <felipe.rita@dcomp.sor.ufscar.br>
2019-05-16 23:07:31 -0300

    remove fields

--- src/renderer/components/Home/Search.vue
+++ src/renderer/components/Home/Search.vue
@@ -1,7 +1,6 @@
 <template>
   <section class="search">
     <list-component
-      :fields="fields"
       :list="list" />
     <div class="text-center more">
       <button
@@ -16,42 +15,12 @@
 
 <script>
 import { mapGetters, mapActions } from 'vuex'
-import moment from 'moment'
 import ListComponent from '@/components/commons/List'
 
 export default {
   name: 'Search',
   components: {
     ListComponent
-  },
-  data () {
-    return {
-      fields: [
-        {
-          title: 'Titulo',
-          key: 'snippet.title'
-        },
-        {
-          title: 'Canal',
-          value: item => {
-            const min = item.snippet.channelTitle.substr(0, 15)
-            if (min !== item.snippet.channelTitle) {
-              return min + '...'
-            }
-            return min
-          }
-        },
-        {
-          title: 'Duração',
-          value: item => {
-            const dur = moment.duration(item.contentDetails.duration)
-            const min = dur.minutes().toString().padStart(2, '0')
-            const sec = dur.seconds().toString().padStart(2, '0')
-            return `${min}:${sec}`
-          }
-        }
-      ]
-    }
   },
   methods: {
     ...mapActions('Search', ['nextPage'])
